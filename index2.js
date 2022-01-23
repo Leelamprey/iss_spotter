5fd30a40660df020cb95168fd53281c54fbcc535
Lee Benzacar <LMBenzacar@gmail.com>
2022-01-23 18:47:52 +0000

    updated for promises

---
+++ index2.js
@@ -0,0 +1,18 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+/* If Passtimes is needed, unsure? Didnt see it in the assignment code, maybe I did something wrong?
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${dateTime} for ${duration} seconds!`);
+  }
+};
+**/
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
